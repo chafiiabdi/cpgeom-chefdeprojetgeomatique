1027c2d624f61c4dc83b2d6a610eca0a02dd2cbf
Chafii Abdi Ahmed <kaderchafii2021@gmail.com>
2026-07-08 10:12:03 +0200

    ajout theme sphinx read the docs

---
+++ source/conf.py
@@ -0,0 +1,30 @@
+﻿project = "Documentation CPGEOM"
+author = "Chafii Abdi Ahmed"
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.githubpages",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+master_doc = "index"
+language = "fr"
+
+html_theme = "sphinx_rtd_theme"
+html_title = "Documentation CPGEOM"
+html_short_title = "CPGEOM"
+
+html_theme_options = {
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "prev_next_buttons_location": "both",
+    "style_external_links": True,
+}
+
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
